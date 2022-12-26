74d4c6ee1cc056df470cedaf92fd89a7b0f2c828
AndreyKo <andreikokorenko@yandex.by>
2022-12-26 22:44:46 +0300

    feat(componets): add barrel pattern

---
+++ src/componets/index.ts
@@ -0,0 +1,10 @@
+import { CustomNavLink } from "./CustomNavLink/CustomNavLink";
+import { RequareAuth } from "./RequareAuth/RequareAuth";
+import { Navigation } from "./Navigation/Navigation";
+import { Search } from "./Search/Search";
+import { Header } from "./Header/Header";
+import { Input } from "./Input/Input";
+import { SignInForm } from "./SignInForm/SignInForm";
+import { SignUpForm } from "./SignUpForm/SignUpForm";
+
+export { CustomNavLink, RequareAuth, Navigation, Search, Header, Input, SignInForm, SignUpForm };
