00ac3c78a62fafdbe5136bd5bdde090779405d8c
AndreyKo <andreikokorenko@yandex.by>
2022-12-26 22:25:29 +0300

    build(assets): add icons and image

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+import { ReactComponent as LogoIcon } from "../assets/icons/Pixemalogo.svg";
+import { ReactComponent as HomeIcon } from "../assets/icons/HomeIcon.svg";
+import { ReactComponent as TrendsIcon } from "../assets/icons/TrendsIcon.svg";
+import { ReactComponent as FavoritesIcon } from "../assets/icons/FavoritesIcon.svg";
+import { ReactComponent as SettingIcon } from "../assets/icons/SettingIcon.svg";
+import authBackground from "./images/authBackground.jpg";
+
+export { LogoIcon, HomeIcon, TrendsIcon, FavoritesIcon, SettingIcon, authBackground };
