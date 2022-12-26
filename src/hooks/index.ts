8c938768eaa6e04c08e564c1b8a60419d749a8aa
AndreyKo <andreikokorenko@yandex.by>
2022-12-26 22:27:56 +0300

    feat(hooks): add new hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,7 @@
 import { useInput } from "./useInput";
 import { useDebounce } from "./useDebounce";
 import { useToggle } from "./useToggle";
+import { useWindowSize } from "./UseWindowSize";
+import { useTheme } from "./useTheme";
 
-export { useInput, useDebounce, useToggle };
+export { useInput, useDebounce, useToggle, useWindowSize, useTheme };
