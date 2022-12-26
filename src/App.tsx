81b80320d3aa7f811fb4a26a1ea90d9dd65fea35
AndreyKo <andreikokorenko@yandex.by>
2022-12-26 22:47:16 +0300

    feat(app): change app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { typography, Wrapper } from "./ui";
+import { ColorMode } from "componets/ColorMode/ColorMode";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 export const App = () => {
-  const [theme, setTheme] = useState("light");
-
-  const handleTheme = () => {
-    setTheme((theme) => (theme === "dark" ? "light" : "dark"));
-  };
-
-  useEffect(() => {
-    document.documentElement.setAttribute("theme", theme);
-  }, [theme]);
-
   return (
-    <Wrapper>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quo, harum facere
-        nostrum quas iste, autem voluptas consequuntur, officia magnam temporibus voluptate veniam
-        recusandae molestiae veritatis nam eum corrupti praesentium?
-      </Text>
-      <button onClick={handleTheme}>Click me</button>
-    </Wrapper>
+    <>
+      <ColorMode />
+      <RouterProvider router={router} />
+    </>
   );
 };
-
-const Text = styled.p`
-  ${typography.H1}
-`;
