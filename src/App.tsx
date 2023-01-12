a84aa99705e97695389639b4be37ce54fef3074d
AndreyKo <andreikokorenko@yandex.by>
2023-01-12 18:20:37 +0300

    docs(app): refactor app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { ColorMode } from "componets/ColorMode/ColorMode";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
 export const App = () => {
-  return (
-    <>
-      <ColorMode />
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
