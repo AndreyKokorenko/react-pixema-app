cf29d0fdcccf767435b9a93d685299c46b348432
AndreyKo <andreikokorenko@yandex.by>
2023-01-12 19:01:08 +0300

    feat(componets): add MainMovies componet

---
+++ src/components/MainMovies/MainMovies.tsx
@@ -0,0 +1,24 @@
+import { MovieList } from "./styles";
+import { useWindowSize } from "hooks";
+import { breakpoint } from "ui";
+import { IMovieSearch } from "types";
+import { MovieTile } from "components";
+
+interface IProps {
+  movies: IMovieSearch[];
+}
+
+export const MainMovies = ({ movies }: IProps) => {
+  const { width } = useWindowSize();
+  return (
+    <MovieList>
+      {width && width > breakpoint.SM
+        ? movies?.map((movie: IMovieSearch) => {
+          return <MovieTile key={movie.imdbID} {...movie} />;
+        })
+        : movies?.map((movie: IMovieSearch) => {
+          return <MovieTile key={movie.imdbID} {...movie} />;
+        })}
+    </MovieList>
+  );
+};
---
+++ src/components/MainMovies/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { screen } from "ui";
+
+const MovieList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+
+  width: calc(100vw - 350px);
+  padding-bottom: 10px;
+
+  ${screen.MD} {
+    width: 80vw;
+  }
+
+  ${screen.SM} {
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+  } ;
+`;
+
+export { MovieList };
