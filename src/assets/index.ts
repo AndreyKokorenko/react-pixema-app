5c4455edd65229a623e4d51690283d2e5e6784b2
AndreyKo <andreikokorenko@yandex.by>
2023-01-12 18:15:22 +0300

    docs(assets) add icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,29 @@
-import { ReactComponent as LogoIcon } from "../assets/icons/Pixemalogo.svg";
-import { ReactComponent as HomeIcon } from "../assets/icons/HomeIcon.svg";
-import { ReactComponent as TrendsIcon } from "../assets/icons/TrendsIcon.svg";
-import { ReactComponent as FavoritesIcon } from "../assets/icons/FavoritesIcon.svg";
-import { ReactComponent as SettingIcon } from "../assets/icons/SettingIcon.svg";
+import { ReactComponent as LogoIcon } from "../assets/icons/pixemalogo.svg";
+import { ReactComponent as HomeIcon } from "../assets/icons/home.svg";
+import { ReactComponent as TrendsIcon } from "../assets/icons/trends.svg";
+import { ReactComponent as FavoritesIcon } from "../assets/icons/favorites.svg";
+import { ReactComponent as SettingIcon } from "../assets/icons/setting.svg";
+import { ReactComponent as IMDbLogo } from "./icons/imdbLogo.svg";
+import { ReactComponent as BookmarkIcon } from "./icons/bookmark.svg";
+import { ReactComponent as ShareIcon } from "./icons/share.svg";
 import authBackground from "./images/authBackground.jpg";
+import { ReactComponent as UserIcon } from "./icons/user.svg";
+import { ReactComponent as DeleteIcon } from "./icons/delete.svg";
+import { ReactComponent as PencilIcon } from "./icons/pencil.svg";
+import { ReactComponent as EmptyFavoritesIcon } from "./icons/emptyFavorites.svg";
 
-export { LogoIcon, HomeIcon, TrendsIcon, FavoritesIcon, SettingIcon, authBackground };
+export {
+  EmptyFavoritesIcon,
+  PencilIcon,
+  UserIcon,
+  LogoIcon,
+  HomeIcon,
+  TrendsIcon,
+  FavoritesIcon,
+  SettingIcon,
+  authBackground,
+  IMDbLogo,
+  BookmarkIcon,
+  ShareIcon,
+  DeleteIcon,
+};
