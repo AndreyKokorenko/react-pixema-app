9cd4bc8f7e14910a9fb95814854dfc911d8d9460
AndreyKo <andreikokorenko@yandex.by>
2023-01-12 18:32:24 +0300

    feat(services): add services file

---
+++ src/services/movieAPI.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { IMovieAPI, IMovieDetailsAPI, Param } from "types";
+import { IParams } from "store";
+
+class MovieAPI {
+  private readonly BASE_URL = process.env.REACT_APP_MOVIE_BASE_URL as string;
+
+  private readonly API = axios.create({
+    baseURL: this.BASE_URL,
+  });
+
+  public async getByTitle(title: string): Promise<IMovieAPI> {
+    const params = {
+      [Param.Search]: title,
+      [Param.Page]: 1,
+    };
+
+    const { data } = await this.API.get<IMovieAPI>("", { params });
+    return data;
+  }
+
+  public async getByFilters(param: IParams): Promise<IMovieAPI> {
+    const params = {
+      [Param.Search]: param.title,
+      [Param.Page]: param.page,
+      [Param.Type]: param.filters.type,
+      [Param.Year]: param.filters.year,
+    };
+
+    const { data } = await this.API.get<IMovieAPI>("", { params });
+    return data;
+  }
+
+  public async getDetailsByID(imdbID: string) {
+    const params = {
+      i: imdbID,
+    };
+    const { data } = await this.API.get<IMovieDetailsAPI>("", {
+      params,
+    });
+
+    return data;
+  }
+}
+
+export const movieAPI = new MovieAPI();
