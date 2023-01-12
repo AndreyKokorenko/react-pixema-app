cbfaecaf5b6af5ab2509124c26ad1dd391b2f859
AndreyKo <andreikokorenko@yandex.by>
2023-01-12 18:22:20 +0300

    docs(index): refactor index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { store } from "store";
 import { App } from "./App";
-import { store } from "./store";
-import { GlobalStyles } from "./ui";
+import { GlobalStyles } from "ui";
+import "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
